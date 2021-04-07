27856ac9b7322cd5b429e280d0aa09a89d258141
Andrew Sik-On Leung <Andrew.sLeung@gmail.com>
2021-04-07 14:55:07 -0400

    Creating Classifier Model
    Completed the Python script to automate the creation of a classification model for the 36 categories based on the messages column of the disaster alert database.
    
    Inputs:
    
    1) User specifies database to pull from
    2) Name for saving model as a pickle file
    
    ML Pipeline:
    
    1) Load cleaned data
    2) Define tokenizer and StartingVerbExtractor() transformer
    3) Create scikit-learn pipline and GridSearchCV objects
    4) Fit model
    5) Evaluate predictions for all 36 models using classification_report
    6) Output model to pickle

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,128 @@
+# import libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.base import BaseEstimator, TransformerMixin
+import joblib
+import time
+
+import numpy as np
+import re
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('messages_categorized',engine)
+    X = df["message"]
+    Y = df.iloc[:,4:]
+    category_names = list(Y.columns)
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    
+    return clean_tokens
+    
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    
+        
+    def starting_verb(self,text):
+        sentence_list = nltk.sent_tokenize(text)
+        #print(sentence_list)
+        for sentence in sentence_list:
+            #print(sentence)
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            #return pos_tags
+            if not pos_tags:
+                return False
+            else:
+                first_word, first_tag = pos_tags[0]
 
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+            return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        X_tagged = X_tagged.fillna(False)
+        return pd.DataFrame(X_tagged)
 
 def build_model():
-    pass
+    pipeline3 = Pipeline([
+    ('features', FeatureUnion([
 
+        ('text_pipeline', Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer())
+        ])),
 
+        ('starting_verb', StartingVerbExtractor())
+    ])),
+
+    ('clf',  MultiOutputClassifier(RandomForestClassifier(max_features='log2')))
+    ])
+    
+    parameters ={
+        'clf__estimator__min_samples_split':[2,4,6,8],
+        'clf__estimator__max_features':['auto', 'sqrt', 'log2']
+
+    }
+    
+    
+    cv = GridSearchCV(pipeline3, param_grid=parameters)
+    
+    return cv
+    
+   
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    Y_pred = model.predict(X_test)
+    for i in range(Y_pred.shape[1]):
+        labels = np.unique(Y_pred[i]).tolist()
+        targets = [str(i) for i in labels]
+        print("model {}".format(category_names[i]))
+        print(classification_report(Y_test.iloc[:,i],Y_pred[:,i], labels=labels, target_names=targets))
+
+    
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    joblib_file = model_filepath
+    joblib.dump(model, joblib_file)
+
 
 
 def main():
