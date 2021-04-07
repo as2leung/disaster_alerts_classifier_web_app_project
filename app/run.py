abc7709587cb50fc9ab3ef452d861358827d04b6
Andrew Sik-On Leung <Andrew.sLeung@gmail.com>
2021-04-07 15:27:57 -0400

    Editing APP.py paths and adding Visualizations
    Changes made
    
    1) Added the StartingVerbExtractor() transformer to allow model pickle file to work
    
    2) Update relative paths to database and model pickle
    
    3) Add visualization for Distribution of Message by Reponse/Outcome
    
    4) Add visualization for Percentage Distribution of Message Genres
    
    5) Updated host address to 127.0.0.1 to use on local environment

--- app/run.py
+++ app/run.py
@@ -1,3 +1,20 @@
+import sys
+import nltk
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+
+
 import json
 import plotly
 import pandas as pd
@@ -8,9 +25,12 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Pie
+#from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
+nltk.download('averaged_perceptron_tagger')
 
 app = Flask(__name__)
 
@@ -25,12 +45,39 @@ def tokenize(text):
 
     return clean_tokens
 
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    
+        
+    def starting_verb(self,text):
+        sentence_list = nltk.sent_tokenize(text)
+        #print(sentence_list)
+        for sentence in sentence_list:
+            #print(sentence)
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            #return pos_tags
+            if not pos_tags:
+                return False
+            else:
+                first_word, first_tag = pos_tags[0]
+
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+            return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        X_tagged = X_tagged.fillna(False)
+        return pd.DataFrame(X_tagged)
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/disaster_alerts.db')
+df = pd.read_sql_table('messages_categorized', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -43,6 +90,11 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    values_per = df['genre'].value_counts(normalize= True).round(3).multiply(100).tolist()
+    labels_per_name = df['genre'].value_counts().index.tolist()
+    
+    color=np.array(['rgb(55, 83, 109)']*df.iloc[:,4:].shape[0])
+    color2=np.array(['rgb(255, 40, 109)']*df.iloc[:,4:].shape[0])
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,7 +115,40 @@ def index():
                     'title': "Genre"
                 }
             }
-        }
+        },
+         {
+            'data': [
+                Bar(
+                    x=list(df.iloc[:,4:].columns),
+                    y=df.iloc[:,4:].sum(),
+                    marker=dict(color=color.tolist())
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message by Reponse/Outcome',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Reponse/Output Variables"
+                }
+            }
+        },
+            {
+                'data': [
+                   
+                      Pie(labels=labels_per_name, values=values_per, textinfo='label+percent'
+                            )
+                ],
+
+                'layout': {
+                    'title': 'Percentage Distribution of Message Genres',
+                    
+
+                }
+            }
+        
     ]
     
     # encode plotly graphs in JSON
@@ -93,7 +178,7 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='127.0.0.1', port=3001, debug=True)
 
 
 if __name__ == '__main__':
